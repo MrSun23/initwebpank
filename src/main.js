808d7c121a447cafb3bab85fec75ebba1340b3f0
MrSun23 <965990981@qq.com>
2017-06-13 00:20:30 +0800

    修正main.js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,4 @@ import template from './numberlist.hbs';
 
 let numbers = map ([1, 2, 3, 4, 5, 6], n => n*n);
 
-document.querySelector('#app-container').innerHTML =  template({ number });
+document.querySelector('#app-container').innerHTML =  template({ numbers });
