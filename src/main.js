b6bfcebad83388f2220a4435565bee97dfae0d3a
MrSun23 <965990981@qq.com>
2017-06-13 23:22:04 +0800

    延迟加载

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import {map} from 'lodash';
-import template from './numberlist.hbs';
+//import template from './numberlist.hbs';
 
 let numbers = map ([1, 2, 3, 4, 5, 6], n => n*n);
 
-document.querySelector('#app-container').innerHTML = template({ numbers });
+setTimeout(() => {
+  require(['./numberlist.hbs'], template => {
+      document.querySelector('#app-container').innerHTML = template({ numbers });
+  });
+}, 2000)
